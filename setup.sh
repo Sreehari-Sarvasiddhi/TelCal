81f6a083a039449a79a9849750eb3ffe56092508
SreehariCharan Sarvasiddhi <sreeharicharan96@gmail.com>
2024-10-05 14:04:51 +0530

    feat: setup script changes

--- setup.sh
+++ setup.sh
@@ -4,23 +4,7 @@ sudo apt install default-jdk -y
 
 java -version
 
-wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
-
-
-sudo tar xf apache-maven-3.9.9-bin.tar.gz -C /opt
-
-# Set JAVA_HOME
-export PATH=/usr/lib/jvm/default-java/bin:$PATH
-
-# Set MAVEN_HOME (Maven)
-export PATH=/opt/apache-maven-3.9.9/bin:$PATH
-
-# Display the variables for confirmation
-echo "PATH is set to: $PATH"
-
-sudo chmod +x ~/.bashrc
-
-source ~/.bashrc
+sudo apt install maven
 
 mvn -version
 
