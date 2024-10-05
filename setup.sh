ca1984e5b8e1edcf40409128f3442b6ed7ff2118
SreehariCharan Sarvasiddhi <sreeharicharan96@gmail.com>
2024-10-05 09:38:26 +0530

    feat: setup file changes

--- setup.sh
+++ setup.sh
@@ -10,18 +10,13 @@ wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bi
 sudo tar xf apache-maven-3.9.9-bin.tar.gz -C /opt
 
 # Set JAVA_HOME
-export JAVA_HOME=/usr/lib/jvm/default-java
-export PATH=$JAVA_HOME/bin:$PATH
+export PATH=/usr/lib/jvm/default-java/bin:$PATH
 
 # Set MAVEN_HOME (Maven)
-export MAVEN_HOME=/opt/apache-maven-3.9.9
-export M3_HOME=$MAVEN_HOME
-export PATH=$MAVEN_HOME/bin:$PATH
+export PATH=/opt/apache-maven-3.9.9/bin:$PATH
 
 # Display the variables for confirmation
-echo "JAVA_HOME is set to: $JAVA_HOME"
-echo "MAVEN_HOME is set to: $MAVEN_HOME"
-echo "M3_HOME is set to: $M3_HOME"
+echo "PATH is set to: $PATH"
 
 sudo chmod +x ~/.bashrc
 
